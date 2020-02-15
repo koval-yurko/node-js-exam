3beaae9ae73d3410f13cf787a292d6e1d7648733
Yurii Kovalchuk <yurii.kovalchuk@sisense.com>
2020-02-15 17:24:00 +0200

    module test utils + process & os

--- process.js
+++ process.js
@@ -8,3 +8,42 @@
 // process.argv[3] - b
 
 
+process.on('unhandledRejection', (err) => {
+    // here the 1 is a file descriptor for STDERR
+    // process.exit([code])
+    // https://nodejs.org/api/process.html#process_exit_codes
+    process.exit(1);
+  })
+
+// SIGTERM
+// SIGUSR1   kill -USR1 PID_OF_THE_NODE_JS_PROCESS
+// https://nodejs.org/api/process.html#process_signal_events
+
+process.on('SIGTERM', function onSigterm () {
+    console.info('Got SIGTERM. Graceful shutdown start', new Date().toISOString())
+    // start graceul shutdown here
+    server.close(function onServerClosed (err) {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        }
+
+        closeMyResources(function onResourcesClosed (err) {
+          // error handling
+          process.exit()
+        })
+      })
+  })
+
+process.cwd()
+
+process.env
+
+//  can send any POSIX signals to any processes
+process.kill(pid, [signal])
+
+// Docker signaling
+// For example in our Alpine image: CMD ["node", "src"] works, CMD ["npm", "start"]
+
+os.platform() // darwin freebsd linux openbsd win32
+os.type() // Linux Darwin Windows_NT
---
+++ testUtils.js
@@ -0,0 +1,87 @@
+const path = require('path');
+
+/**
+ * @module
+ * Contains utilities that helps in unit testing
+ */
+
+function getStack(){
+    const orig = Error.prepareStackTrace;
+    Error.prepareStackTrace = function(_, stack){ return stack; };
+    const err = new Error;
+    Error.captureStackTrace(err, arguments.callee);
+    const stack = err.stack;
+    Error.prepareStackTrace = orig;
+    return stack;
+}
+
+/**
+ * Returns the path to the module from which caller function were called.
+ */
+function getModulePathBasedOnParentModule(module){
+    const modulePath = module.indexOf('/') >= 0 || module.indexOf(path.sep) >= 0 ? path.join(path.dirname(getStack()[2].getFileName()), module) : module;
+    return require.resolve(modulePath);
+}
+
+module.exports = {
+
+
+    /**
+     * Removes module from node cache. Next time module will be required, node will reload it.
+     * NOTE!! Doesn't work with node native modules such as fs, path, etc
+     * @param {string} module - module name or path to the module
+     * @returns {boolean} - returns true if module were removed, false otherwise.
+     * (NOTE! if module wasn't loaded by node before calling this function false will be returned since there is nothing to remove)
+     */
+    removeModuleFromCache(module){
+        return delete require.cache[getModulePathBasedOnParentModule(module)];
+    },
+
+    /**
+     * Replaces loaded module with passed module. If module wasn't loaded this function will force node to load the module and then replace loaded module with passed one.
+     * NOTE!! Doesn't work with node native modules such as fs, path, etc
+     * @param {string} module - module name or path
+     * @param {Object|Function} newModule - new module's object/function
+     * @returns {{restore: (function()), getPreviousModule()}} - object with restore function which rolls back changes and replaces passed module with original module in cache and the previousModule which was in the cache
+     */
+    replaceModuleInCache(module, newModule){
+        const modulePath = getModulePathBasedOnParentModule(module);
+        let prevModule = require.cache[modulePath] && require.cache[modulePath].exports;
+
+        if (!prevModule) {
+            require.cache[modulePath] = {};
+        }
+
+        require.cache[modulePath].exports = newModule;
+        return {
+            /**
+             * Rolls back the changes
+             */
+            restore(){
+                if (prevModule) {
+                    require.cache[modulePath].exports = prevModule;
+                } else {
+                    delete require.cache[modulePath];
+                }
+            },
+            /**
+             * Returns the previous module if it exists
+             * @returns {Object} - the previous module which was in the cache
+             */
+            getPreviousModule(){
+                return prevModule;
+            }
+        };
+    },
+
+    /**
+     * Replaces loaded external module with passed module. If module wasn't loaded this function will force node to load the module and then replace loaded module with passed one.
+     * NOTE!! external module must have index.js file
+     * @param {string} module - module name
+     * @param {Object|Function} newModule - new module's object/function
+     * @returns {{restore: (function()), getPreviousModule()}} - object with restore function which rolls back changes and replaces passed module with original module in cache and the previousModule which was in the cache
+     */
+    replaceExternalModuleInCache(module, newModule){
+        return this.replaceModuleInCache(`../node_modules/${module}`, newModule);
+    }
+};
